3b37cec3a36f9eea3713b95f4bfb164920b77c58
CryptoDebug <axel10.lemaitre@gmail.com>
2023-11-15 13:19:45 +0100

    update

--- hangman-web/go.mod
+++ hangman-web/go.mod
@@ -1,3 +1,4 @@
-module hanmganweb
+module hangman-web
 
 go 1.21.0
+require hangman-web/server v1.0.0
--- hangman-web/server/server/template.go
+++ hangman-web/server/server/template.go
@@ -5,20 +5,23 @@ import (
 	"net/http"
 )
 
-
 type Page struct {
 	PageTitle string
-	Mot string
+	Mot       string
 }
 
 func Server(word string) {
 	tmpl := template.Must(template.ParseFiles("test.html"))
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        data := Page {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := Page{
 			PageTitle: "Test html",
-			Mot: word,
-			}
+			Mot:       word,
+		}
 		tmpl.Execute(w, data)
-	})    
-    http.ListenAndServe(":8080", nil)
+	})
+	http.ListenAndServe(":8080", nil)
 }
+
+/*go.mod: module server
+
+go 1.21.0*/
