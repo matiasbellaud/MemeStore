dd57d155d0bd1fd42bd7545a0040f5362449db91
kelyan.danis@ynov.com <kelyan.danis@ynov.com>
2023-12-20 14:24:30 +0100

    📝 add documentation for userinformation 📝, 🐛 i also add security for non writable caractére un password, like space 🐛

--- back/handler/userinformation.go
+++ back/handler/userinformation.go
@@ -5,6 +5,7 @@ import (
     "regexp"
 )
 
+// set the news errors case used by the foloming functions
 var (
 	IncorectDescription = errors.New("Description do not match whith the rules")
 	IncorectMail = errors.New("Mail do not match whith a corectly made mail adress")
@@ -14,6 +15,21 @@ var (
 )
 
 func CorectInformation(description, mail, username, password, verificaztionword string) error {
+	/*
+	func Corectinformation verify if the information put by the user in the form are in the same form as we need, a valid mail adress, 
+	a good password...
+	--------------------------------------------------------------
+	input : description, mail  username, password, verificationword as string and also the user information
+	output : error case
+	--------------------------------------------------------------
+	step by step the function see if the user information match whith the regexp gived to him, especialy for mail.
+	error case from the regexp.Matching handel
+	if the information dosen't match whith regexp, the function return the error in the information
+
+	after the func pass all the test it return a nil error, because the information dosent have one
+
+	! the password dosen't use regexp rule but an other function write down there !
+	*/
 	matched, err := regexp.MatchString(`.`, description)
 	if err != nil {
 		return err
@@ -53,9 +69,27 @@ func CorectInformation(description, mail, username, password, verificaztionword 
 }
 
 func GoodPassword(password string) (goodPassword bool, err error) {
+	/*
+	the function GoodPassword verify if the password put are folowing our rules for password
+	--------------------------------------------
+	input : the password as string
+	output : the boolean representing if the password folowing the rules, ans also error case
+	--------------------------------------------
+	at first, the programe verify if the password length is superior or equal to 12, if the passwors lenth is lower than 12 it return 
+	the password error
+
+	next the function set the variables used to count the password lowerkase, upercase, digit, special caractéres
+	the function walk acrose the password letter by letter and if the caracére is in one of the 4 family of letter it add 1 to the count of 
+	it family, the fucntion use regexp to determine if letter is in on of the 4 family.
+	if the letter dosen't bee in the 4 family the program return password error and false
+
+	once the letter see one by one, whe return true if the password have 1 or + upercase and lowercase and special caractére and 4 or + digit,
+	the program return nil error.
+	*/
 	if len(password) < 12 {
 		return false, nil
 	}
+
 	lowercaseLetter := 0
 	upercaseLetter := 0
 	digit := 0
@@ -68,6 +102,7 @@ func GoodPassword(password string) (goodPassword bool, err error) {
 		}
 		if matched {
 			lowercaseLetter += 1
+			continue
 		}
 		matched, err = regexp.MatchString(`[A-Z]`, string(password[caractere]))
 		if err != nil {
@@ -75,6 +110,7 @@ func GoodPassword(password string) (goodPassword bool, err error) {
 		}
 		if matched {
 			upercaseLetter += 1
+			continue
 		}
 		matched, err = regexp.MatchString(`[0-9]`, string(password[caractere]))
 		if err != nil {
@@ -82,6 +118,7 @@ func GoodPassword(password string) (goodPassword bool, err error) {
 		}
 		if matched {
 			digit += 1
+			continue
 		}
 		matched =  byte(33) <=password[caractere] && password[caractere]<= byte(47) || 
 		byte(58) <=password[caractere] && password[caractere]<= byte(64) || 
@@ -89,11 +126,9 @@ func GoodPassword(password string) (goodPassword bool, err error) {
 		byte(123) <=password[caractere] && password[caractere]<= byte(126)
 		if matched {
 			specialCaractere += 1
+			continue
 		}
-	}
-	if lowercaseLetter > 0 && upercaseLetter > 0 && digit >= 4 && specialCaractere >= 0{
-		return true, nil
-	} else {
 		return false, nil
 	}
+	return lowercaseLetter > 0 && upercaseLetter > 0 && digit >= 4 && specialCaractere >= 0 , nil
 }
