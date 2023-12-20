949c3115ad1942e55afa980157294ec82e66752f
kelyan.danis@ynov.com <kelyan.danis@ynov.com>
2023-12-20 08:13:28 +0100

    📝 i add documentation for PasswordFunc 📝

--- back/handler/PasswordFunc.go
+++ back/handler/PasswordFunc.go
@@ -11,6 +11,7 @@ import (
 	"golang.org/x/crypto/argon2"
 )
 
+// initialasing the structure used for hashing
 type constants struct {
 	memory uint32
 	iterations uint32
@@ -19,6 +20,7 @@ type constants struct {
 	parallelism uint8
 }
 
+// set the var use in all folowing func, first for errors second to the hashing method
 var (
 	ErrIncompatibleVersion = errors.New("incompatible version of argon2")
 	ErrInvalidHash = errors.New("The hash is'nt in wrigth format")
@@ -32,6 +34,20 @@ var (
 )
 
 func HasingFunc(password string) (hash string, err error) {
+	/*
+	principal function to hash pasword
+	-------------------------------------------------------
+	input : the password a string
+	output : the formated hash (version,key,hash) and potential error
+	-------------------------------------------------------
+	at first the function générate a key to make the argon2 hash
+	if the key generation faild the function return an empty string and the error
+
+	after that func hash the password and then encode in Base64 the key and the hash
+	finaly function create a string who contain the argon version, the base 64 encripted key and base64 encrypted hash
+
+	at the end return a final string and a nil error
+	*/
 	key, err := RandomKey(conste.keyLength)
 	if err != nil {
 		return "",err
@@ -48,6 +64,18 @@ func HasingFunc(password string) (hash string, err error) {
 }
 
 func RandomKey(length uint32) ([]byte, error) {
+	/*
+	function who generate random key for argon2 hash
+	-------------------------------------------------
+	input : an unit 32 represent the lenght of the key
+	output : the key in byte format, potential error
+	-------------------------------------------------
+	first step the func create the array of byte whith good length
+	next it randomise the content of the array
+	if the randomising fail func return a nil array and the error
+
+	at the end it return the key in array of byte form and a nil error
+	*/
 	key := make([]byte, length)
 	_, err := rand.Read(key)
 	if err != nil {
@@ -57,6 +85,20 @@ func RandomKey(length uint32) ([]byte, error) {
 }
 
 func PasswordEqualHash(password, Hash string) (info bool, err error) {
+	/*
+	function who compare a password to a stocked hash
+	---------------------------------------------------
+	input : the password and stocked hash the both are string
+	output : the boolean born from the comparing and possible error
+	---------------------------------------------------
+	first the func use the ReadHash func
+	if the ReadHash return an error the func return false and the error
+	
+	after the func use the same key to the password
+	if the two hash are the same, func return true and a nil error
+
+	at the end func returne false and a nil error cose password is different than the hash
+	*/
 	key, hash, err := ReadHash(Hash)
 	if err != nil {
 		return false, err
@@ -71,6 +113,24 @@ func PasswordEqualHash(password, Hash string) (info bool, err error) {
 }
 
 func ReadHash(StockHash string) (key, hash []byte, err error) {
+	/*
+	ReadHash function take a stocked hash an return it
+	-----------------------------------------------------
+	input : a stock hash a type string
+	output : the function return the hash key and hash as array of byte and an error if the function fail
+	-----------------------------------------------------
+	first the func extract the information stocked in the stockhash
+	func verifie if the number of information is equal to 4, return an error if not
+
+	func verifie if the curent version of argon is the same used for the stock hash, if the both version arent the same func return an error
+
+	func decode the hash key encode in B64 and see if the length of the key is the same as parametres, return an error if key have diferent length
+	than the params
+	
+	func decode B64 hash and see if is length is the same as params, return an error if not
+
+	after those test the func return the key, hash and a nil err
+	*/
 	values := strings.Split(StockHash, "$")
 	if len(values) != 4 {
 		return nil, nil, ErrInvalidHash
